043d8dcfb3424d27f4a522b7cfeefd34e8ee9df7
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-08-21 00:19:53 +0700

    :lipstick: Thai Typography

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,23 +6,30 @@ module.exports = {
   port: 3457,
   locales: {
     '/': {
-      lang: 'th'
-    }
+      lang: 'th',
+    },
   },
   markdown: {
     config: md => {
       md.use(require('markdown-it-footnote'))
+      md.renderer.rules.softbreak = () =>
+        '<span class="เว้นวรรคใหญ่">&#10;</span>'
+      md.renderer.rules.text = (original =>
+        function() {
+          const text = original.apply(this, arguments)
+          return text.replace(/ๆ/g, '<span class="ๆ">ๆ</span>')
+        })(md.renderer.rules.text)
       md.renderer.render = (original =>
         function() {
           return twemoji.parse(original.apply(this, arguments))
         })(md.renderer.render)
-    }
+    },
   },
   themeConfig: {
     logo: '/resources/logo.svg',
     // sidebar: [
     // ],
     search: false,
-    nav: [{ text: 'หน้าแรก', link: '/' }]
-  }
+    nav: [{ text: 'หน้าแรก', link: '/' }],
+  },
 }
