ce2854494aa79f6e76707813be5a2802774533af
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-09-11 23:20:22 +0700

    visitors could use some greetings

--- .vuepress/theme/Layout.vue
+++ .vuepress/theme/Layout.vue
@@ -4,6 +4,13 @@
       <template slot="page-bottom">
         <Utterances :path="$page.path" :title="$page.title" />
       </template>
+      <template slot="sidebar-top">
+        <div class="wondersoft-sidebar-contents">
+          <div class="wondersoft-sidebar-contents-smiley">
+            hello!<br />:)
+          </div>
+        </div>
+      </template>
     </DefaultThemeLayout>
   </div>
 </template>
@@ -18,3 +25,22 @@ export default {
   components: { DefaultThemeLayout, Utterances }
 }
 </script>
+
+<style lang="stylus">
+.wondersoft-sidebar-contents {
+  position: absolute;
+  top: 3.6rem;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .wondersoft-sidebar-contents-smiley {
+    opacity: 0.5;
+    font-size: 5em;
+  }
+}
+</style>
