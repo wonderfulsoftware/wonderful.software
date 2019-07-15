b0ae9c1e55635b701cfcefcbade96c664ebb6084
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-07-15 21:48:58 +0700

    Add override for Page

---
+++ .vuepress/theme/components/Page.vue
@@ -0,0 +1,15 @@
+<template>
+  <DefaultThemePage :sidebarItems="sidebarItems"></DefaultThemePage>
+</template>
+
+<script>
+import DefaultThemePage from '@parent-theme/components/Page.vue'
+
+export default {
+  props: ['sidebarItems'],
+  components: { DefaultThemePage },
+}
+</script>
+
+<style>
+</style>
