a4481b7944f0d308878c9228da97742a5b0d8927
dtinth on Cloud9 <dtinth@spacet.me>
2020-11-08 18:15:02 +0000

    Add littlefoot

--- .vuepress/theme/components/Page.vue
+++ .vuepress/theme/components/Page.vue
@@ -1,15 +1,67 @@
 <template>
-  <DefaultThemePage :sidebarItems="sidebarItems"></DefaultThemePage>
+  <DefaultThemePage
+    :sidebarItems="sidebarItems"
+    @click="onClick"
+  ></DefaultThemePage>
 </template>
 
 <script>
 import DefaultThemePage from '@parent-theme/components/Page.vue'
+import littlefoot from 'littlefoot'
+import 'littlefoot/dist/littlefoot.css'
+
+function setupFootnotes() {
+  littlefoot({
+    buttonTemplate: `<button
+  aria-expanded="false"
+  aria-label="Footnote <% number %>"
+  class="littlefoot__button"
+  id="<% reference %>"
+  title="See Footnote <% number %>"
+/>
+  <% number %>
+</button>`,
+  })
+}
 
 export default {
   props: ['sidebarItems'],
   components: { DefaultThemePage },
+  mounted() {
+    setupFootnotes()
+  },
+  updated() {
+    setupFootnotes()
+  },
+  methods: {
+    onClick() {},
+  },
 }
 </script>
 
 <style>
+.wondersoft .littlefoot__button {
+  position: relative;
+  display: inline-block;
+  font: inherit;
+  font-size: 80%;
+  color: #da3567;
+  appearance: none;
+  cursor: pointer;
+  background: transparent;
+  border: 1px solid #da3567;
+  padding: 0;
+  border-radius: 1.25em;
+  width: 1.25em;
+  height: 1.25em;
+  line-height: 1em;
+  margin-left: 0.5ex;
+}
+.wondersoft .footnotes-sep {
+  display: none;
+}
+.wondersoft .littlefoot-footnote__content {
+  word-wrap: break-word;
+  display: block;
+}
 </style>
