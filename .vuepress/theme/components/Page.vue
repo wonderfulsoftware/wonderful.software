ccd7d183b6e234dc91e9fc88c86f0a93089e24f6
Thai Pangsakulyanont <dtinth@spacet.me>
2020-12-27 10:59:21 +0000

    Fix footnotes

--- .vuepress/theme/components/Page.vue
+++ .vuepress/theme/components/Page.vue
@@ -7,68 +7,19 @@
 
 <script>
 import DefaultThemePage from '@parent-theme/components/Page.vue'
-import littlefoot from 'littlefoot'
-import 'littlefoot/dist/littlefoot.css'
-
-let latestLf
-
-function setupFootnotes() {
-  if (latestLf) {
-    latestLf.unmount()
-  }
-  latestLf = littlefoot({
-    buttonTemplate: `<button
-  aria-expanded="false"
-  aria-label="Footnote <% number %>"
-  class="littlefoot__button"
-  id="<% reference %>"
-  title="See Footnote <% number %>"
-/>
-  <% number %>
-</button>`,
-  })
-}
-
-window.setupFootnotes = setupFootnotes
+import { setupInteractiveFootnotes } from '../util/InteractiveFootnotes'
 
 export default {
   props: ['sidebarItems'],
   components: { DefaultThemePage },
   mounted() {
-    setupFootnotes()
+    setupInteractiveFootnotes()
   },
   updated() {
-    setupFootnotes()
+    setupInteractiveFootnotes()
   },
   methods: {
     onClick() {},
   },
 }
 </script>
-
-<style>
-.wondersoft .littlefoot__button {
-  position: relative;
-  display: inline-block;
-  font: inherit;
-  font-size: 80%;
-  color: #da3567;
-  appearance: none;
-  cursor: pointer;
-  background: transparent;
-  border: 1px solid #da3567;
-  padding: 0;
-  border-radius: 1.25em;
-  width: 1.25em;
-  height: 1.25em;
-  line-height: 1em;
-  margin-left: 0.5ex;
-}
-.wondersoft .footnotes-sep {
-  display: none;
-}
-.wondersoft .littlefoot-footnote__content {
-  word-wrap: break-word;
-  display: block;
-}
-</style>
