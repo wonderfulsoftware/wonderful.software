b2bc54152c3563c764147f4e92823e7c7d0cf439
dtinth on MBP M1 <dtinth@spacet.me>
2023-08-03 21:37:32 +0700

    sidebar refactors

---
+++ js/jsSidebar.ts
@@ -0,0 +1,30 @@
+import type { DefaultTheme } from 'vitepress'
+export const jsSidebar: DefaultTheme.SidebarItem[] = [
+  {
+    text: '[DRAFT] Intro to Programming with JavaScript',
+    link: '/js/',
+    items: [
+      {
+        text: '0. Introduction',
+        link: '/js/intro/',
+      },
+      {
+        text: '1. Basic Calculation',
+        items: [
+          {
+            text: 'Basic Calculation Exercises',
+            link: '/js/exercises/basic-calculation/',
+          },
+        ],
+      },
+      {
+        text: 'Appendices',
+        items: [
+          {
+            // TODO
+          },
+        ],
+      },
+    ],
+  },
+]
