3fea10cf4cf886fcd426488ef18f6842675d1817
Thai Pangsakulyanont <dtinth@spacet.me>
2023-08-06 13:50:24 +0000

    Update index.md and jsSidebar.ts

--- js/index.md
+++ js/index.md
@@ -3,3 +3,12 @@
 :::warning DRAFT
 This is a draft content and may be incomplete or inaccurate.
 :::
+
+:::tip Legend
+
+- ⠀ = almost no content / placeholder
+- ⠤ = some rough ideas written out
+- ⠶ = unfinished writing
+- ⠿ = draft
+
+:::
--- js/jsSidebar.ts
+++ js/jsSidebar.ts
@@ -3,28 +3,29 @@ export const jsSidebar: DefaultTheme.SidebarItem[] = [
   {
     text: '[DRAFT] Intro to Programming with JavaScript',
     link: '/js/',
+    // Progress -> ⠀ ⠤ ⠶ ⠿
     items: [
       {
         text: '0. Introduction',
         items: [
           {
-            text: 'เขียนโปรแกรมเป็นแล้วทำอะไรได้',
+            text: '⠿ เขียนโปรแกรมเป็นแล้วทำอะไรได้',
             link: '/js/intro.html',
           },
           {
-            text: 'ตัวอย่างการใช้ JavaScript Console ในชีวิตประจำวัน',
+            text: '⠶ ตัวอย่างการใช้ JavaScript Console ในชีวิตประจำวัน',
             link: '/js/daily-life-console.html',
           },
           {
-            text: 'พื้นฐานที่ควรมี',
+            text: '⠿ พื้นฐานที่ควรมี',
             link: '/js/prerequisites.html',
           },
           {
-            text: 'คอร์สนี้จะได้เรียนรู้อะไรบ้าง',
+            text: '⠤ คอร์สนี้จะได้เรียนรู้อะไรบ้าง',
             link: '/js/scope.html',
           },
           {
-            text: 'การเรียนรู้การเขียนโปรแกรม',
+            text: '⠿ การเรียนรู้การเขียนโปรแกรม',
             link: '/js/learning.html',
           },
         ],
@@ -33,20 +34,20 @@ export const jsSidebar: DefaultTheme.SidebarItem[] = [
         text: '1. ทำความรู้จักกับภาษา JavaScript',
         items: [
           {
-            text: 'การคำนวณเบื้องต้น',
+            text: '⠀ การคำนวณเบื้องต้น',
           },
           {
-            text: '[แบบฝึกหัด] การคำนวณเบื้องต้น',
+            text: '⠶ [แบบฝึกหัด] การคำนวณเบื้องต้น',
             link: '/js/exercises/basic-calculation.html',
           },
           {
-            text: 'ตัวเลข ข้อความ และตัวดำเนินการ (Numbers, Strings, Operators)',
+            text: '⠀ ตัวเลข ข้อความ และตัวดำเนินการ (Numbers, Strings, Operators)',
           },
           {
-            text: 'ตัวแปร (Variables)',
+            text: '⠀ ตัวแปร (Variables)',
           },
           {
-            text: 'ฟังก์ชัน (Functions)',
+            text: '⠀ ฟังก์ชัน (Functions)',
           },
         ],
       },
@@ -54,16 +55,16 @@ export const jsSidebar: DefaultTheme.SidebarItem[] = [
         text: '2. ลองสร้างเว็บแอปพลิเคชัน',
         items: [
           {
-            text: 'แนะนำเว็บ Glitch',
+            text: '⠀ แนะนำเว็บ Glitch',
           },
           {
-            text: 'HTML แบบเบื้องต้นสุดๆ',
+            text: '⠀ HTML แบบเบื้องต้นสุดๆ',
           },
           {
-            text: 'Input',
+            text: '⠀ Input',
           },
           {
-            text: 'สร้างเว็บไซต์ของตัวเองบน Neocities',
+            text: '⠀ สร้างเว็บไซต์ของตัวเองบน Neocities',
           },
         ],
       },
@@ -71,13 +72,13 @@ export const jsSidebar: DefaultTheme.SidebarItem[] = [
         text: '3. โครงสร้างภาษา',
         items: [
           {
-            text: 'เงื่อนไข (Conditionals): if, else, else if',
+            text: '⠀ เงื่อนไข (Conditionals): if, else, else if',
           },
           {
-            text: 'เงื่อนไขแบบหลายตัวเลือก: switch, case',
+            text: '⠀ เงื่อนไขแบบหลายตัวเลือก: switch, case',
           },
           {
-            text: 'การทำซ้ำ (Loops): while, for',
+            text: '⠀ การทำซ้ำ (Loops): while, for',
           },
         ],
       },
@@ -85,23 +86,23 @@ export const jsSidebar: DefaultTheme.SidebarItem[] = [
         text: 'บทเสริม',
         items: [
           {
-            text: 'การพิมพ์ดีด',
+            text: '⠶ การพิมพ์ดีด',
             link: '/js/typing.html',
           },
           {
-            text: 'ความสำคัญของชั่วโมงบิน',
+            text: '⠤ ความสำคัญของชั่วโมงบิน',
             link: '/js/quantity.html',
           },
           {
-            text: 'ทำไมถึงเลือกภาษา JavaScript',
+            text: '⠀ ทำไมถึงเลือกภาษา JavaScript',
             link: '/js/why.html',
           },
           {
-            text: 'เหตุเกิดจากความขี้เกียจ',
+            text: '⠀ เหตุเกิดจากความขี้เกียจ',
             link: '/js/lazy.html',
           },
           {
-            text: 'แหล่งเรียนรู้อื่นๆ',
+            text: '⠶ แหล่งเรียนรู้อื่นๆ',
             link: '/js/resources.html',
           },
         ],
